1fc4719bf761cd3b13104f48c46311e4960c283c
Nikolas Diam <NikolasDmn>
2025-12-08 20:11:15 +0100

    Day 8 massive optimizations

--- 08/part1.rs
+++ 08/part1.rs
@@ -1,62 +1,129 @@
 use aoc;
 use itertools::Itertools;
-use rayon::prelude::*;
-use std::collections::HashSet;
+use std::collections::{BinaryHeap, HashMap};
 
 const EXPECTED: usize = 40;
 
 aoc::solution!(EXPECTED, solve);
 
-fn solve(input: &str) -> usize {
-    let mut all_nodes = input
+use std::cmp::Ordering;
+// A custom struct to hold edge info inside the Heap
+#[derive(PartialEq, Debug)]
+struct Edge {
+    dist_sq: i64, // Using i64 to match your exact logic
+    u: usize,
+    v: usize,
+}
+
+impl Eq for Edge {}
+
+impl PartialOrd for Edge {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.dist_sq.partial_cmp(&other.dist_sq)
+    }
+}
+
+impl Ord for Edge {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.dist_sq
+            .partial_cmp(&other.dist_sq)
+            .unwrap_or(Ordering::Equal)
+    }
+}
+
+struct UnionFind {
+    parent: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+        }
+    }
+    fn find(&mut self, i: usize) -> usize {
+        if self.parent[i] == i {
+            i
+        } else {
+            let root = self.find(self.parent[i]);
+            self.parent[i] = root;
+            root
+        }
+    }
+    fn union(&mut self, i: usize, j: usize) {
+        let root_i = self.find(i);
+        let root_j = self.find(j);
+        if root_i != root_j {
+            self.parent[root_i] = root_j;
+        }
+    }
+}
+
+pub fn solve(input: &str) -> usize {
+    let points: Vec<(i64, i64, i64)> = input
         .lines()
         .map(|line| {
-            let coords: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
-            (coords[0], coords[1], coords[2])
+            let mut nums = line.split(',').map(|s| s.trim().parse::<i64>().unwrap());
+            (
+                nums.next().unwrap(),
+                nums.next().unwrap(),
+                nums.next().unwrap(),
+            )
         })
-        .tuple_combinations()
-        .map(|((x1, y1, z1), (x2, y2, z2))| {
-            let x_diff = x1 as f64 - x2 as f64;
-            let y_diff = y1 as f64 - y2 as f64;
-            let z_diff = z1 as f64 - z2 as f64;
+        .collect();
 
-            let dist = (x_diff.powi(2) + y_diff.powi(2) + z_diff.powi(2)).sqrt();
-            ((x1, y1, z1), (x2, y2, z2), dist)
-        })
-        .collect::<Vec<((i32, i32, i32), (i32, i32, i32), f64)>>();
-    let nodes = if all_nodes.len() < 1000 {
-        all_nodes
-            .select_nth_unstable_by(10, |a, b| a.2.total_cmp(&b.2))
-            .0
-    } else {
-        all_nodes
-            .select_nth_unstable_by(1000, |a, b| a.2.total_cmp(&b.2))
-            .0
-    };
-    nodes.sort_by(|a, b| a.2.total_cmp(&b.2));
-    nodes
-        .iter()
-        .fold(vec![], |acc: Vec<HashSet<(i32, i32, i32)>>, (c1, c2, _)| {
-            let (to_merge, mut others): (Vec<_>, Vec<_>) = acc
-                .into_iter()
-                .partition(|set| set.contains(c1) || set.contains(c2));
+    let n = points.len();
 
-            let mut new_group = HashSet::new();
-            new_group.insert(*c1);
-            new_group.insert(*c2);
+    let total_edges = n * (n - 1) / 2;
+    let limit = if total_edges < 1000 { 10 } else { 1000 };
 
-            for set in to_merge {
-                new_group.extend(set);
+    // limit + 1 to never realocate when pushing
+    let mut heap: BinaryHeap<Edge> = BinaryHeap::with_capacity(limit + 1);
+
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let (x1, y1, z1) = points[i];
+            let (x2, y2, z2) = points[j];
+
+            let dx = (x1 - x2) as i64;
+            let dy = (y1 - y2) as i64;
+            let dz = (z1 - z2) as i64;
+            let dist = dx * dx + dy * dy + dz * dz;
+
+            // worse than the worst edge in our heap, skip it immediately.
+            if heap.len() == limit {
+                if let Some(worst_best) = heap.peek() {
+                    if dist >= worst_best.dist_sq {
+                        continue;
+                    }
+                }
             }
 
-            others.push(new_group);
-            others
-        })
-        .into_iter()
-        .map(|c| c.len())
-        .collect::<Vec<usize>>()
-        .select_nth_unstable_by(3, |a, b| b.cmp(a))
-        .0
-        .iter()
+            heap.push(Edge {
+                dist_sq: dist,
+                u: i,
+                v: j,
+            });
+
+            if heap.len() > limit {
+                heap.pop();
+            }
+        }
+    }
+
+    let mut dsu = UnionFind::new(n);
+    for edge in heap {
+        dsu.union(edge.u, edge.v);
+    }
+
+    let mut group_counts = HashMap::new();
+    for i in 0..n {
+        *group_counts.entry(dsu.find(i)).or_insert(0) += 1;
+    }
+
+    group_counts
+        .values()
+        .sorted_unstable_by(|a, b| b.cmp(a))
+        .take(3)
         .product()
 }
--- 08/part2.rs
+++ 08/part2.rs
@@ -1,48 +1,69 @@
 use aoc;
 use itertools::Itertools;
-use std::collections::HashSet;
+use rayon::prelude::*;
 
 const EXPECTED: usize = 25272;
 
 aoc::solution!(EXPECTED, solve);
 
-fn solve(input: &str) -> usize {
-    let nodes = input.lines().map(|line| {
-        let coords: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
-        (coords[0], coords[1], coords[2])
-    });
-    let num_nodes = nodes.clone().count();
-    let mut node_combinations = nodes
-        .tuple_combinations()
-        .map(|((x1, y1, z1), (x2, y2, z2))| {
-            let x_diff = x1 as f64 - x2 as f64;
-            let y_diff = y1 as f64 - y2 as f64;
-            let z_diff = z1 as f64 - z2 as f64;
-
-            let dist = (x_diff.powi(2) + y_diff.powi(2) + z_diff.powi(2)).sqrt();
-            ((x1, y1, z1), (x2, y2, z2), dist)
+pub fn solve(input: &str) -> usize {
+    let points: Vec<[i64; 3]> = input
+        .lines()
+        .map(|line| {
+            let mut nums = line.split(',').map(|s| s.trim().parse::<i64>().unwrap());
+            [
+                nums.next().unwrap(),
+                nums.next().unwrap(),
+                nums.next().unwrap(),
+            ]
         })
-        .collect::<Vec<((i32, i32, i32), (i32, i32, i32), f64)>>();
-    node_combinations.sort_by(|a, b| a.2.total_cmp(&b.2));
-    let mut networks = vec![];
-    for (c1, c2, _) in node_combinations.iter() {
-        let (to_merge, mut others): (Vec<HashSet<_>>, Vec<HashSet<_>>) = networks
-            .into_iter()
-            .partition(|set: &HashSet<_>| set.contains(&c1) || set.contains(&c2));
+        .collect();
 
-        let mut new_group = HashSet::new();
-        new_group.insert(c1);
-        new_group.insert(c2);
+    let n = points.len();
 
-        for set in to_merge {
-            new_group.extend(set);
+    let mut min_dists = vec![i64::MAX; n];
+    let mut parents = vec![0; n];
+    let mut visited = vec![false; n];
+
+    min_dists[0] = 0;
+    for _ in 0..n {
+        let mut best_candidate_index = usize::MAX;
+        let mut min_val = i64::MAX;
+
+        for i in 0..n {
+            if !visited[i] && min_dists[i] < min_val {
+                min_val = min_dists[i];
+                best_candidate_index = i;
+            }
         }
-
-        if new_group.len() == num_nodes {
-            return c1.0 as usize * c2.0 as usize;
+        if best_candidate_index == usize::MAX {
+            break;
         }
-        others.push(new_group);
-        networks = others;
+        visited[best_candidate_index] = true;
+        for neighbor_index in 0..n {
+            if !visited[neighbor_index] {
+                let (x1, y1, z1) = (points[best_candidate_index][0], points[best_candidate_index][1], points[best_candidate_index][2]);
+                let (x2, y2, z2) = (points[neighbor_index][0], points[neighbor_index][1], points[neighbor_index][2]);
+                let dx = x1 - x2;
+                let dy = y1 - y2;
+                let dz = z1 - z2;
+                let dist = dx * dx + dy * dy + dz * dz;
+
+                if dist < min_dists[neighbor_index] {
+                    min_dists[neighbor_index] = dist;
+                    parents[neighbor_index] = best_candidate_index; 
+                }
+            }
+        }
     }
-    0
+
+    //Connector piece is the node with the maximum edge weight in the MST
+    let max_weight_node = min_dists
+        .iter()
+        .position_max_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+
+    let max_weight_parent = parents[max_weight_node];
+
+    (points[max_weight_node][0] as usize) * (points[max_weight_parent][0] as usize)
 }
